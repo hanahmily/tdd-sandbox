afbfa055785c6826f9c7ccef3d7cf86c6ac47264
Hongtao Gao <hanahmily@gmail.com>
2026-07-23 08:42:28 +0000

    vajra(implement): implement for 20260723-082851-addapackaged

--- difference/difference.go
+++ difference/difference.go
@@ -2,10 +2,6 @@
 package difference
 
 // Sub returns the difference a - b.
-//
-// This is an unimplemented RED stub: it compiles so the package builds, but
-// panics at runtime so the accompanying unit and end-to-end tests fail until a
-// real implementation is provided.
 func Sub(a, b int) int {
-	panic("difference.Sub not implemented")
+	return a - b
 }
